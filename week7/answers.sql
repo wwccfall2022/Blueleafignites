bb5e5dd56470eaefe633d7b12b4832fcc2c186b2
Rachel Mettler <82110851+Blueleafignites@users.noreply.github.com>
2022-11-03 08:42:59 -0700

    Created rest of the tables

--- week7/answers.sql
+++ week7/answers.sql
@@ -18,5 +18,86 @@ CREATE TABLE characters (
   FOREIGN KEY (player_id)
   REFERENCES players (player_id)
   ON UPDATE CASCADE
+  ON DELETE CASCADE
+);
+
+CREATE TABLE winners (
+  character_id INT UNSIGNED,
+  name VARCHAR(30) NOT NULL
+  
+  FOREIGN KEY (character_id)
+  REFERENCES characters (character_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+);
+
+CREATE TABLE character_stats (
+  character_id INT UNSIGNED,
+  health TINYINT NOT NULL,
+  armor TINYINT NOT NULL
+  
+  FOREIGN KEY (character_id)
+  REFERENCES characters (character_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+);
+
+CREATE TABLE teams (
+  team_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30)
+);
+
+CREATE TABLE team_members (
+  team_member_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  team_id INT UNSIGNED,
+  character_id INT UNSIGNED
+  
+  FOREIGN KEY (team_id)
+  REFERENCES teams (team_id)
+  ON UPDATE CASCADE
   ON DELETE CASCADE,
+  
+  FOREIGN KEY (character_id)
+  REFERENCES characters (character_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+);
+
+CREATE TABLE items (
+  item_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  armor INT UNSIGNED,
+  damage INT UNSIGNED
+);
+
+CREATE TABLE inventory (
+  inventory_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  character_id INT UNSIGNED,
+  item_id INT UNSIGNED
+  
+  FOREIGN KEY (character_id)
+  REFERENCES characters (character_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
+  
+  FOREIGN KEY (item_id)
+  REFERENCES items (item_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+);
+
+CREATE TABLE equipped (
+  equipped_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  character_id INT UNSIGNED,
+  item_id INT UNSIGNED
+  
+  FOREIGN KEY (character_id)
+  REFERENCES characters (character_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
+  
+  FOREIGN KEY (item_id)
+  REFERENCES items (item_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
 );
