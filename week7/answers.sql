9fe3a765d704728608e43df206f993d58a2d71d9
Rachel Mettler <82110851+Blueleafignites@users.noreply.github.com>
2022-11-03 08:28:10 -0700

    Added characters table

--- week7/answers.sql
+++ week7/answers.sql
@@ -7,5 +7,16 @@ CREATE TABLE players (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL
-  );
+);
   
+CREATE TABLE characters (
+  character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  player_id INT UNSIGNED,
+  name VARCHAR(30) NOT NULL,
+  level INT UNSIGNED
+  
+  FOREIGN KEY (player_id)
+  REFERENCES players (player_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
+);
