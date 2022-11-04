6fdc0db9d44a887207bf540ac4854e403e77ef3b
Rachel Mettler <82110851+Blueleafignites@users.noreply.github.com>
2022-11-04 15:01:46 -0700

    Created the equip procedure

--- week7/answers.sql
+++ week7/answers.sql
@@ -13,22 +13,24 @@ CREATE TABLE characters (
   character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   player_id INT UNSIGNED,
   name VARCHAR(30) NOT NULL,
-  level INT UNSIGNED,
+  level TINYINT,
   
-  FOREIGN KEY (player_id)
-  REFERENCES players (player_id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE
+  CONSTRAINT characters_fk_players
+    FOREIGN KEY (player_id)
+      REFERENCES players (player_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
 );
 
 CREATE TABLE winners (
   character_id INT UNSIGNED,
   name VARCHAR(30) NOT NULL,
   
+ CONSTRAINT winners_fk_characters
   FOREIGN KEY (character_id)
-  REFERENCES characters (character_id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE
+    REFERENCES characters (character_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
 CREATE TABLE character_stats (
@@ -36,10 +38,11 @@ CREATE TABLE character_stats (
   health TINYINT NOT NULL,
   armor TINYINT NOT NULL,
   
-  FOREIGN KEY (character_id)
-  REFERENCES characters (character_id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE
+  CONSTRAINT character_stats_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
 );
 
 CREATE TABLE teams (
@@ -52,15 +55,17 @@ CREATE TABLE team_members (
   team_id INT UNSIGNED,
   character_id INT UNSIGNED,
   
-  FOREIGN KEY (team_id)
-  REFERENCES teams (team_id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE,
+  CONSTRAINT team_members_fk_teams
+    FOREIGN KEY (team_id)
+      REFERENCES teams (team_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
   
-  FOREIGN KEY (character_id)
-  REFERENCES characters (character_id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE
+  CONSTRAINT team_members_fk_characters
+    FOREIGN KEY (character_id)
+      REFERENCES characters (character_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
 );
 
 CREATE TABLE items (
@@ -75,15 +80,17 @@ CREATE TABLE inventory (
   character_id INT UNSIGNED,
   item_id INT UNSIGNED,
   
-  FOREIGN KEY (character_id)
-  REFERENCES characters (character_id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE,
+  CONSTRAINT inventory_fk_characters
+    FOREIGN KEY (character_id)
+      REFERENCES characters (character_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
   
-  FOREIGN KEY (item_id)
-  REFERENCES items (item_id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE
+  CONSTRAINT inventory_fk_items
+    FOREIGN KEY (item_id)
+      REFERENCES items (item_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
 );
 
 CREATE TABLE equipped (
@@ -91,13 +98,33 @@ CREATE TABLE equipped (
   character_id INT UNSIGNED,
   item_id INT UNSIGNED,
   
-  FOREIGN KEY (character_id)
-  REFERENCES characters (character_id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE,
+  CONSTRAINT equipped_fk_characters
+    FOREIGN KEY (character_id)
+      REFERENCES characters (character_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
   
-  FOREIGN KEY (item_id)
-  REFERENCES items (item_id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE
+  CONSTRAINT equipped_fk_items
+    FOREIGN KEY (item_id)
+      REFERENCES items (item_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
 );
+
+DELIMITER ;;
+CREATE PROCEDURE equip(inventory_id INT UNSIGNED)
+BEGIN
+	DECLARE find_item_by_id INT UNSIGNED;
+    SELECT inventory_id INTO find_item_by_id;
+    
+	INSERT INTO equipped
+	SELECT 
+		inventory.inventory_id,
+		inventory.character_id,
+		inventory.item_id
+	FROM inventory
+	WHERE inventory.inventory_id = find_item_by_id;
+    
+	DELETE FROM inventory WHERE inventory.inventory_id = find_item_by_id; 
+END;;
+DELIMITER ;
