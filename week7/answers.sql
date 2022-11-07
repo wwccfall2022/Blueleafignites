5ac4e1727187aecbb66ab59a4ad564031cdd4576
Rachel Mettler <82110851+Blueleafignites@users.noreply.github.com>
2022-11-07 11:46:32 -0800

    Added work in progress of the attack procedure

--- week7/answers.sql
+++ week7/answers.sql
@@ -3,112 +3,112 @@ CREATE SCHEMA destruction;
 USE destruction;
 
 CREATE TABLE players (
-  player_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL
+	player_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	email VARCHAR(50) NOT NULL
 );
   
 CREATE TABLE characters (
-  character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  player_id INT UNSIGNED,
-  name VARCHAR(30) NOT NULL,
-  level TINYINT,
-  
-  CONSTRAINT characters_fk_players
-    FOREIGN KEY (player_id)
-      REFERENCES players (player_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
+	character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	player_id INT UNSIGNED,
+	name VARCHAR(30) NOT NULL,
+	level TINYINT,
+
+	CONSTRAINT characters_fk_players
+		FOREIGN KEY (player_id)
+		REFERENCES players (player_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE winners (
-  character_id INT UNSIGNED,
-  name VARCHAR(30) NOT NULL,
-  
- CONSTRAINT winners_fk_characters
-  FOREIGN KEY (character_id)
-    REFERENCES characters (character_id)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
+	character_id INT UNSIGNED,
+	name VARCHAR(30) NOT NULL,
+
+	CONSTRAINT winners_fk_characters
+		FOREIGN KEY (character_id)
+		REFERENCES characters (character_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE character_stats (
-  character_id INT UNSIGNED,
-  health TINYINT NOT NULL,
-  armor TINYINT NOT NULL,
-  
-  CONSTRAINT character_stats_fk_characters
-    FOREIGN KEY (character_id)
-    REFERENCES characters (character_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
+	character_id INT UNSIGNED,
+	health TINYINT NOT NULL,
+	armor TINYINT NOT NULL,
+
+	CONSTRAINT character_stats_fk_characters
+		FOREIGN KEY (character_id)
+		REFERENCES characters (character_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE teams (
-  team_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30)
+	team_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30)
 );
 
 CREATE TABLE team_members (
-  team_member_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  team_id INT UNSIGNED,
-  character_id INT UNSIGNED,
+	team_member_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	team_id INT UNSIGNED,
+	character_id INT UNSIGNED,
   
-  CONSTRAINT team_members_fk_teams
-    FOREIGN KEY (team_id)
-      REFERENCES teams (team_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE,
+	CONSTRAINT team_members_fk_teams
+		FOREIGN KEY (team_id)
+		REFERENCES teams (team_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
   
-  CONSTRAINT team_members_fk_characters
-    FOREIGN KEY (character_id)
-      REFERENCES characters (character_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
+	CONSTRAINT team_members_fk_characters
+		FOREIGN KEY (character_id)
+		REFERENCES characters (character_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE items (
-  item_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30),
-  armor INT UNSIGNED NOT NULL,
-  damage INT UNSIGNED NOT NULL
+	item_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30),
+	armor INT UNSIGNED NOT NULL,
+	damage INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE inventory (
-  inventory_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  character_id INT UNSIGNED,
-  item_id INT UNSIGNED,
+	inventory_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	character_id INT UNSIGNED,
+	item_id INT UNSIGNED,
   
-  CONSTRAINT inventory_fk_characters
-    FOREIGN KEY (character_id)
-      REFERENCES characters (character_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE,
+	CONSTRAINT inventory_fk_characters
+		FOREIGN KEY (character_id)
+		REFERENCES characters (character_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
   
-  CONSTRAINT inventory_fk_items
-    FOREIGN KEY (item_id)
-      REFERENCES items (item_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
+	CONSTRAINT inventory_fk_items
+		FOREIGN KEY (item_id)
+		REFERENCES items (item_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE equipped (
-  equipped_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  character_id INT UNSIGNED,
-  item_id INT UNSIGNED,
+	equipped_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	character_id INT UNSIGNED,
+	item_id INT UNSIGNED,
   
-  CONSTRAINT equipped_fk_characters
-    FOREIGN KEY (character_id)
-      REFERENCES characters (character_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE,
+	CONSTRAINT equipped_fk_characters
+		FOREIGN KEY (character_id)
+		REFERENCES characters (character_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
   
-  CONSTRAINT equipped_fk_items
-    FOREIGN KEY (item_id)
-      REFERENCES items (item_id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
+	CONSTRAINT equipped_fk_items
+		FOREIGN KEY (item_id)
+		REFERENCES items (item_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 DELIMITER ;;
@@ -169,8 +169,8 @@ DETERMINISTIC
 BEGIN
 	DECLARE find_armor_total_by_character_id INT UNSIGNED;
 	DECLARE sum_of_character_armor_from_items TINYINT;
-    DECLARE sum_of_character_natural_armor TINYINT;
-    DECLARE total_sum_of_armor TINYINT;
+	DECLARE sum_of_character_natural_armor TINYINT;
+	DECLARE total_sum_of_armor TINYINT;
     
 	SELECT character_id INTO find_armor_total_by_character_id;
     
@@ -180,15 +180,48 @@ BEGIN
 		INNER JOIN items
 			ON equipped.item_id = items.item_id
 	WHERE equipped.character_id = find_armor_total_by_character_id
-    INTO sum_of_character_armor_from_items;
+    	INTO sum_of_character_armor_from_items;
     
 	SELECT character_stats.armor 
-    FROM character_stats 
-    WHERE character_stats.character_id = find_armor_total_by_character_id
-    INTO sum_of_character_natural_armor;
+	FROM character_stats 
+	WHERE character_stats.character_id = find_armor_total_by_character_id
+	INTO sum_of_character_natural_armor;
 
 	SELECT sum_of_character_armor_from_items + sum_of_character_natural_armor INTO total_sum_of_armor;
 
-    Return total_sum_of_armor;
+Return total_sum_of_armor;
+END;;
+
+CREATE PROCEDURE attack(id_of_character_being_attacked INT UNSIGNED, id_of_equipped_item_used_for_attack INT UNSIGNED)
+BEGIN
+	DECLARE find_total_dmg_by_item_id INT UNSIGNED;	
+	DECLARE character_being_attacked INT UNSIGNED;
+	DECLARE total_dmg TINYINT;
+	DECLARE character_hp TINYINT;
+
+	SELECT id_of_equipped_item_used_for_attack INTO find_total_dmg_by_item_id;
+	SELECT id_of_character_being_attacked INTO character_being_attacked;
+    
+	SELECT 
+		SUM(items.damage)
+	FROM equipped
+		INNER JOIN items
+			ON equipped.item_id = items.item_id
+	WHERE equipped.item_id = find_total_dmg_by_item_id
+    	INTO total_dmg;
+    
+    	SELECT 
+		character_stats.health
+	FROM characters
+		INNER JOIN character_stats
+			ON characters.character_id = character_stats.character_id
+	WHERE characters.character_id = character_being_attacked
+    	INTO character_hp;
+    
+    	IF (armor_total(character_being_attacked) - total_dmg) < 0 THEN
+		IF ((character_hp + armor_total(character_being_attacked)) - total_dmg) <= 0 THEN
+			DELETE FROM characters WHERE characters.character_id = character_being_attacked;
+		END IF;
+   	END IF;
 END;;
 DELIMITER ;
